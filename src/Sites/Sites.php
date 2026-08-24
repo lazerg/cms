58a9e54698f46cf6a4ed9cab78be3275ddc4d0a9
Jay George <contact@jaygeorge.co.uk>
2026-08-24 15:05:59 +0100

    Fix site grouping PHPUnit failures
    Keep group keys before attributes when saving sites and update Site augmentation expectations.

--- src/Sites/Sites.php
+++ src/Sites/Sites.php
@@ -537,8 +537,17 @@ class Sites
             }
 
             if ($groupName) {
+                unset($site['group'], $site['group_handle']);
+
+                $attributes = $site['attributes'] ?? null;
+                unset($site['attributes']);
+
                 $site['group'] = $groupName;
                 $site['group_handle'] = $groupKey;
+
+                if ($attributes !== null) {
+                    $site['attributes'] = $attributes;
+                }
             } else {
                 unset($site['group'], $site['group_handle']);
             }
